a2f77cd09106f63f0365bd969487f76e2037c31b
Adam Bossy <adambossy@gmail.com>
2025-09-26 12:54:46 -0400

    Remove whitespace

--- libs/db/alembic/env.py
+++ libs/db/alembic/env.py
@@ -18,8 +18,6 @@ from sqlalchemy import engine_from_config, pool
 from dotenv import load_dotenv, find_dotenv
 
 
-
-
 # Alembic Config object, which provides access to the values within
 # the .ini file in use.
 config = context.config
